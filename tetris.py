32607423ffcc8a6ac12cfc072bd17b8fc16edce5
Tristan Angieri <tristan.angieri@gmail.com>
2019-10-03 15:49:19 +0000

    updated tetris.py

--- tetris.py
+++ tetris.py
@@ -15,12 +15,6 @@ In Tetris, really the only object is a Tetromino. It has states of:
         * moving (sideways)
         * rotating
 """
-import time
-
-class Shape:
-
-    def __init__(self, borders):
-        self.borders = borders
 
 class Tetromino:
 
@@ -30,45 +24,3 @@ class Tetromino:
         self.color = color
         self.coordinates = coordinates
         self.isFalling = isFalling
-
-    def getRotation(self):
-        """Get the Tetromino current 'rotation' value"""
-        return self.rotation
-
-    def setRotation(self, value):
-        self.rotation = value
-
-    def getShape(self):
-        """Get the Tetromino 'shape' value"""
-        return self.shape
-
-    def setShape(self, value):
-        self.shape = value
-
-    def getColor(self):
-        """Get the Tetromino 'color' value"""
-        return self.color
-
-    def setColor(self, value):
-        self.color = value
-
-    def getCoordinates(self):
-        """Get The Tetromino 'coordinates' value"""
-        return self.coordinates
-
-    def setCoordinates(self, value):
-        self.coordinates = value
-
-    def getIsFalling(self):
-        """Get the Tetromino 'isFalling' value"""
-        return self.isFalling
-
-    def fall(self):
-        """Decrement the y coordinate of a Tetromino's position while it is falling"""
-        while self.isFalling:
-            time.sleep(.25)
-            self.coordinates[1] -- 1
-
-    def land(self):
-        """Assign a Tetromino's isFalling value to False"""
-        self.isFalling = False
